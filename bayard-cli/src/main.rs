9d3273a8a727ed05fbaaa55b292875efeecc51ba
Minoru Osuka <minoru.osuka@gmail.com>
2020-05-13 10:21:08 +0900

    Merge CLI code

--- bayard-cli/src/main.rs
+++ bayard-cli/src/main.rs
@@ -1,8 +1,8 @@
-use bayard_cli::cli::root::run_root_cli;
+use bayard_cli::cli::command::root;
 use bayard_common::log::set_logger;
 
 fn main() -> Result<(), std::io::Error> {
     set_logger();
 
-    run_root_cli()
+    root()
 }
