8dd177d037f4897577bc61145e15b9f5310d92cd
Minoru Osuka <minoru.osuka@gmail.com>
2020-06-01 12:39:49 +0900

    Migrate to Actix web

--- bayard-rest/src/main.rs
+++ bayard-rest/src/main.rs
@@ -2,12 +2,18 @@
 extern crate clap;
 
 use clap::{App, AppSettings, Arg};
+use crossbeam_channel::select;
+use log::*;
 
 use bayard_common::log::set_logger;
-use bayard_rest::rest::server::{run, run_tls};
+use bayard_common::signal::sigterm_channel;
+use bayard_rest::rest::server::RestServer;
 
-fn main() -> Result<(), std::io::Error> {
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
     set_logger();
+
+    let threads = format!("{}", num_cpus::get().to_owned());
 
     let app = App::new(crate_name!())
         .setting(AppSettings::DeriveDisplayOrder)
@@ -44,38 +50,47 @@ fn main() -> Result<(), std::io::Error> {
                 .default_value("0.0.0.0:5000")
                 .takes_value(true),
         )
-        // .arg(
-        //     Arg::with_name("CORS_ORIGIN")
-        //         .help("Add an origin that are allowed to make requests.")
-        //         .short("o")
-        //         .long("cors-origin")
-        //         .value_name("ORIGIN")
-        //         .takes_value(true),
-        // )
-        // .arg(
-        //     Arg::with_name("CORS_METHODS")
-        //         .help("Set a list of methods which the allowed origins are allowed to access for requests.")
-        //         .short("m")
-        //         .long("cors-method")
-        //         .value_name("METHODS")
-        //         .takes_value(true)
-        //         .multiple(true)
-        //         .use_delimiter(true)
-        //         .require_delimiter(true)
-        //         .value_delimiter(","),
-        // )
-        // .arg(
-        //     Arg::with_name("CORS_HEADERS")
-        //         .help("Set a list of header field names which can be used when this resource is accessed by allowed origins.")
-        //         .short("l")
-        //         .long("cors-headers")
-        //         .value_name("HEADERS")
-        //         .takes_value(true)
-        //         .multiple(true)
-        //         .use_delimiter(true)
-        //         .require_delimiter(true)
-        //         .value_delimiter(","),
-        // )
+        .arg(
+            Arg::with_name("HTTP_WORKER_THREADS")
+                .help("Number of HTTP worker threads. By default http server uses number of available logical cpu as threads count.")
+                .short("w")
+                .long("worker-threads")
+                .value_name("THREADS")
+                .default_value(&threads)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("CORS_ORIGIN")
+                .help("Add an origin that are allowed to make requests.")
+                .short("o")
+                .long("cors-origin")
+                .value_name("ORIGIN")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("CORS_METHODS")
+                .help("Set a list of methods which the allowed origins are allowed to access for requests.")
+                .short("m")
+                .long("cors-method")
+                .value_name("METHODS")
+                .takes_value(true)
+                .multiple(true)
+                .use_delimiter(true)
+                .require_delimiter(true)
+                .value_delimiter(","),
+        )
+        .arg(
+            Arg::with_name("CORS_HEADERS")
+                .help("Set a list of header field names which can be used when this resource is accessed by allowed origins.")
+                .short("l")
+                .long("cors-headers")
+                .value_name("HEADERS")
+                .takes_value(true)
+                .multiple(true)
+                .use_delimiter(true)
+                .require_delimiter(true)
+                .value_delimiter(","),
+        )
         .arg(
             Arg::with_name("CERT_FILE")
                 .help("Path to the TLS certificate file.")
@@ -98,22 +113,27 @@ fn main() -> Result<(), std::io::Error> {
     let host = matches.value_of("HOST").unwrap();
     let port = matches.value_of("PORT").unwrap().parse::<u16>().unwrap();
     let index_address = matches.value_of("INDEX_ADDRESS").unwrap();
-    // let mut cors_origin = "".to_string();
-    // if let Some(_cors_origin) = matches.value_of("CORS_ORIGIN") {
-    //     cors_origin = _cors_origin.to_string();
-    // }
-    // let mut cors_methods = Vec::new();
-    // if let Some(_cors_methods) = matches.values_of("CORS_METHODS") {
-    //     _cors_methods
-    //         .map(|s| cors_methods.push(s.to_string()))
-    //         .count();
-    // }
-    // let mut cors_headers = Vec::new();
-    // if let Some(_cors_headers) = matches.values_of("CORS_HEADERS") {
-    //     _cors_headers
-    //         .map(|s| cors_headers.push(s.to_string()))
-    //         .count();
-    // }
+    let http_worker_threads = matches
+        .value_of("HTTP_WORKER_THREADS")
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let mut cors_origin = "".to_string();
+    if let Some(_cors_origin) = matches.value_of("CORS_ORIGIN") {
+        cors_origin = _cors_origin.to_string();
+    }
+    let mut cors_methods = Vec::new();
+    if let Some(_cors_methods) = matches.values_of("CORS_METHODS") {
+        _cors_methods
+            .map(|s| cors_methods.push(s.to_string()))
+            .count();
+    }
+    let mut cors_headers = Vec::new();
+    if let Some(_cors_headers) = matches.values_of("CORS_HEADERS") {
+        _cors_headers
+            .map(|s| cors_headers.push(s.to_string()))
+            .count();
+    }
     let mut cert_file = "";
     if let Some(_cert_file) = matches.value_of("CERT_FILE") {
         cert_file = _cert_file;
@@ -123,18 +143,64 @@ fn main() -> Result<(), std::io::Error> {
         key_file = _key_file;
     }
 
-    let rest_addr = format!("{}:{}", host, port);
-    let rest_address = rest_addr.as_ref();
+    let rest_address = format!("{}:{}", host, port);
 
-    if cert_file != "" && key_file != "" {
-        if let Err(_e) = run_tls(rest_address, index_address, cert_file, key_file) {
-            std::process::exit(1);
-        }
-    } else {
-        if let Err(_e) = run(rest_address, index_address) {
-            std::process::exit(1);
+    let enable_cors =
+        !cors_origin.is_empty() && !cors_methods.is_empty() && !cors_headers.is_empty();
+    let enable_tls = !cert_file.is_empty() && !key_file.is_empty();
+
+    let mut rest_server = match (enable_tls, enable_cors) {
+        (false, false) => RestServer::new(rest_address.as_str(), index_address, http_worker_threads),
+        (false, true) => RestServer::new_cors(
+            rest_address.as_str(),
+            index_address,
+            http_worker_threads,
+            cors_origin,
+            cors_methods,
+            cors_headers,
+        ),
+        (true, false) => RestServer::new_tls(
+            rest_address.as_str(),
+            index_address,
+            http_worker_threads,
+            cert_file,
+            key_file,
+        ),
+        (true, true) => RestServer::new_cors_tls(
+            rest_address.as_str(),
+            index_address,
+            http_worker_threads,
+            cors_origin,
+            cors_methods,
+            cors_headers,
+            cert_file,
+            key_file,
+        ),
+    };
+    info!("start rest service on {}", rest_address.as_str());
+
+    // Wait for signals for termination (SIGINT, SIGTERM).
+    let sigterm_receiver = sigterm_channel().unwrap();
+    loop {
+        select! {
+            recv(sigterm_receiver) -> _ => {
+                info!("receive signal");
+                break;
+            }
         }
     }
 
-    Ok(())
+    match rest_server.shutdown().await {
+        Ok(_) => {
+            info!("stop rest service on {}:{}", host, port);
+            Ok(())
+        }
+        Err(e) => {
+            error!(
+                "failed to stop rest service on {}:{}: error={}",
+                host, port, e
+            );
+            Err(e)
+        }
+    }
 }
