ebbe1fa1d7b09a5803f2e752c74151acc111a863
Minoru Osuka <minoru.osuka@gmail.com>
2022-08-08 15:10:56 +0900

    Refactor metastore (#175)

--- bayard/src/index/metastore.rs
+++ bayard/src/index/metastore.rs
@@ -1,12 +1,4 @@
-use std::{
-    collections::HashMap,
-    fmt,
-    path::PathBuf,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-};
+use std::{collections::HashMap, fmt, path::PathBuf, sync::Arc};
 
 use crossbeam::channel::{unbounded, TryRecvError};
 use notify::{
@@ -74,11 +66,8 @@ impl MetastoreError {
 }
 
 pub struct Metastore {
-    indices_dir: PathBuf,
     metadatas: Arc<RwLock<HashMap<String, Metadata>>>, // index name -> metadata
-    metadatas_sender: Arc<watch::Sender<HashMap<String, Metadata>>>,
     metadatas_receiver: watch::Receiver<HashMap<String, Metadata>>,
-    is_running: Arc<AtomicBool>,
 }
 
 impl Metastore {
@@ -91,7 +80,8 @@ impl Metastore {
         }
 
         // Load existing index metadatas.
-        let mut metadatas: HashMap<String, Metadata> = HashMap::new();
+        let metadatas: Arc<RwLock<HashMap<String, Metadata>>> =
+            Arc::new(RwLock::new(HashMap::new()));
         let mut indices_read_dir = fs::read_dir(&indices_dir)
             .await
             .map_err(|error| MetastoreErrorKind::DirectoryReadFailure.with_error(error))?;
@@ -100,9 +90,10 @@ impl Metastore {
             .await
             .map_err(|error| MetastoreErrorKind::DirectoryReadFailure.with_error(error))?
         {
+            let mut mut_metadatas = metadatas.write().await;
+
             // Get index directory path.
             let index_dir = index_dir_entry.path();
-            println!("index_dir: {:?}", index_dir);
             if !index_dir.is_dir() {
                 // Skip non-directory entries.
                 continue;
@@ -132,18 +123,124 @@ impl Metastore {
                 }
             };
 
-            metadatas.insert(index_name, metadata);
+            mut_metadatas.insert(index_name, metadata);
         }
 
         let (metadatas_sender, metadatas_receiver) =
-            watch::channel::<HashMap<String, Metadata>>(metadatas.clone());
+            watch::channel::<HashMap<String, Metadata>>(metadatas.read().await.clone());
+
+        let indices_dir_task = indices_dir.clone();
+        let metadatas_task = Arc::clone(&metadatas);
+
+        tokio::spawn(async move {
+            let index_meta_re = Regex::new(INDEX_METADATA_FILE_PATTERN).unwrap();
+
+            let (tx_filesystem, rx_filesystem) = unbounded();
+
+            let mut watcher: RecommendedWatcher =
+                RecommendedWatcher::new(tx_filesystem).expect("Watch error.");
+            watcher
+                .watch(indices_dir_task.as_path(), RecursiveMode::Recursive)
+                .expect("Watch error.");
+
+            loop {
+                match rx_filesystem.try_recv() {
+                    Ok(event) => {
+                        let event = match event {
+                            Ok(event) => event,
+                            Err(error) => {
+                                error!(?error, "Receive error event.");
+                                continue;
+                            }
+                        };
+
+                        let path = match event.paths.first() {
+                            Some(path) => path,
+                            None => {
+                                error!(?event, "No path in event.");
+                                continue;
+                            }
+                        };
+
+                        if index_meta_re.is_match(path.to_str().unwrap_or("")) {
+                            // Extract index name from path.
+                            let index_name = match extract_index_name(path) {
+                                Ok(name) => name,
+                                Err(error) => {
+                                    error!(?path, ?error, "Failed to extract index name.");
+                                    continue;
+                                }
+                            };
+
+                            match event.kind {
+                                EventKind::Modify(ModifyKind::Name(RenameMode::To))
+                                | EventKind::Modify(ModifyKind::Data(DataChange::Any)) => {
+                                    // Load index metadata.
+                                    info!(?path, "Load index metadata.");
+                                    let index_metadata = match load_index_metadata(path).await {
+                                        Ok(index_metadata) => index_metadata,
+                                        Err(error) => {
+                                            error!(?path, ?error, "Failed to load metadata.");
+                                            continue;
+                                        }
+                                    };
+
+                                    // Update index metadatas.
+                                    info!(?index_name, "Insert index metadata.");
+                                    let mut mut_metadatas = metadatas_task.write().await;
+                                    mut_metadatas
+                                        .insert(index_name.clone(), index_metadata.clone());
+
+                                    match metadatas_sender.send(mut_metadatas.clone()) {
+                                        Ok(_) => info!("Sent index metadatas to the stream."),
+                                        Err(_error) => {
+                                            error!(
+                                                ?index_name,
+                                                // ?error,
+                                                "Failed to send index metadatas."
+                                            );
+                                        }
+                                    }
+                                }
+                                EventKind::Remove(RemoveKind::File) => {
+                                    info!(?index_name, "Remove index metadata.");
+                                    let mut mut_metadatas = metadatas_task.write().await;
+                                    mut_metadatas.remove(&index_name);
+
+                                    match metadatas_sender.send(mut_metadatas.clone()) {
+                                        Ok(_) => info!("Sent index metadatas to the stream."),
+                                        Err(_error) => {
+                                            error!(
+                                                ?index_name,
+                                                // ?error,
+                                                "Failed to send index metadatas."
+                                            );
+                                        }
+                                    }
+                                }
+                                _ => {
+                                    debug!(?event, "Unexpected event kind.");
+                                    continue;
+                                }
+                            }
+                        } else {
+                            debug!(?path, "Ignore path.");
+                            continue;
+                        }
+                    }
+                    Err(TryRecvError::Empty) => {
+                        sleep(SLEEP_DURATION).await;
+                    }
+                    Err(TryRecvError::Disconnected) => {
+                        break;
+                    }
+                }
+            }
+        });
 
         Ok(Metastore {
-            indices_dir,
-            metadatas: Arc::new(RwLock::new(metadatas)),
-            metadatas_sender: Arc::new(metadatas_sender),
+            metadatas,
             metadatas_receiver,
-            is_running: Arc::new(AtomicBool::new(false)),
         })
     }
 
@@ -152,125 +249,6 @@ impl Metastore {
     }
 
     pub fn watch(&self) -> WatchStream<HashMap<String, Metadata>> {
-        let indices_dir = self.indices_dir.clone();
-        let metadatas = Arc::clone(&self.metadatas);
-        let metadatas_sender = Arc::clone(&self.metadatas_sender);
-        let is_running = Arc::clone(&self.is_running);
-
-        let (tx_filesystem, rx_filesystem) = unbounded();
-
-        let index_meta_re = Regex::new(INDEX_METADATA_FILE_PATTERN).unwrap();
-
-        if !is_running.load(Ordering::Relaxed) {
-            info!("Start index watcher.");
-            tokio::spawn(async move {
-                is_running.store(true, Ordering::Relaxed);
-
-                let mut watcher: RecommendedWatcher =
-                    RecommendedWatcher::new(tx_filesystem).expect("Watch error.");
-
-                watcher
-                    .watch(indices_dir.as_path(), RecursiveMode::Recursive)
-                    .expect("Watch error.");
-
-                loop {
-                    match rx_filesystem.try_recv() {
-                        Ok(event) => {
-                            let event = match event {
-                                Ok(event) => event,
-                                Err(error) => {
-                                    error!(?error, "Receive error event.");
-                                    continue;
-                                }
-                            };
-
-                            let path = match event.paths.first() {
-                                Some(path) => path,
-                                None => {
-                                    error!(?event, "No path in event.");
-                                    continue;
-                                }
-                            };
-
-                            if index_meta_re.is_match(path.to_str().unwrap_or("")) {
-                                // Extract index name from path.
-                                let index_name = match extract_index_name(path) {
-                                    Ok(name) => name,
-                                    Err(error) => {
-                                        error!(?path, ?error, "Failed to extract index name.");
-                                        continue;
-                                    }
-                                };
-
-                                match event.kind {
-                                    EventKind::Modify(ModifyKind::Name(RenameMode::To))
-                                    | EventKind::Modify(ModifyKind::Data(DataChange::Any)) => {
-                                        // Load index metadata.
-                                        info!(?path, "Load index metadata.");
-                                        let index_metadata = match load_index_metadata(path).await {
-                                            Ok(index_metadata) => index_metadata,
-                                            Err(error) => {
-                                                error!(?path, ?error, "Failed to load metadata.");
-                                                continue;
-                                            }
-                                        };
-
-                                        // Update index metadatas.
-                                        info!(?index_name, "Insert index metadata.");
-                                        let mut metadatas = metadatas.write().await;
-                                        metadatas
-                                            .insert(index_name.clone(), index_metadata.clone());
-
-                                        match metadatas_sender.send(metadatas.clone()) {
-                                            Ok(_) => info!("Sent index metadatas to the stream."),
-                                            Err(_error) => {
-                                                error!(
-                                                    ?index_name,
-                                                    // ?error,
-                                                    "Failed to send index metadatas."
-                                                );
-                                            }
-                                        }
-                                    }
-                                    EventKind::Remove(RemoveKind::File) => {
-                                        info!(?index_name, "Remove index metadata.");
-                                        let mut metadatas = metadatas.write().await;
-                                        metadatas.remove(&index_name);
-
-                                        match metadatas_sender.send(metadatas.clone()) {
-                                            Ok(_) => info!("Sent index metadatas to the stream."),
-                                            Err(_error) => {
-                                                error!(
-                                                    ?index_name,
-                                                    // ?error,
-                                                    "Failed to send index metadatas."
-                                                );
-                                            }
-                                        }
-                                    }
-                                    _ => {
-                                        debug!(?event, "Unexpected event kind.");
-                                        continue;
-                                    }
-                                }
-                            } else {
-                                debug!(?path, "Ignore path.");
-                                continue;
-                            }
-                        }
-                        Err(TryRecvError::Empty) => {
-                            sleep(SLEEP_DURATION).await;
-                        }
-                        Err(TryRecvError::Disconnected) => {
-                            break;
-                        }
-                    }
-                }
-            });
-        } else {
-            info!("Index watcher is already running.");
-        }
-
         WatchStream::new(self.metadatas_receiver.clone())
     }
 }
--- bayard/src/index/shard.rs
+++ bayard/src/index/shard.rs
@@ -1,12 +1,30 @@
 use time::OffsetDateTime;
 
+fn default_state() -> State {
+    State::Unknown
+}
+
 fn default_version() -> i64 {
     OffsetDateTime::now_utc().unix_timestamp()
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub enum State {
+    #[serde(rename = "unknown")]
+    Unknown,
+    #[serde(rename = "serving")]
+    Serving,
+    #[serde(rename = "draining")]
+    Draining,
+    #[serde(rename = "drained")]
+    Drained,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Shard {
     pub id: String,
+    #[serde(default = "default_state")]
+    pub state: State,
     #[serde(default = "default_version")]
     version: i64,
 }
@@ -15,6 +33,7 @@ impl Shard {
     pub fn new(id: String) -> Self {
         Self {
             id,
+            state: State::Serving,
             version: OffsetDateTime::now_utc().unix_timestamp(),
         }
     }
@@ -22,30 +41,87 @@ impl Shard {
 
 #[cfg(test)]
 mod tests {
-    use crate::index::shard::Shard;
+    use crate::index::shard::{Shard, State};
 
     #[test]
     fn test_shard_from_slice() {
         let shard_json_str = r#"
         {
-            "id": "aaaaaaaa"
+            "id": "shard_1"
         }
         "#;
         let shard_json_bytes = shard_json_str.as_bytes();
-
         let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
-        assert_eq!(shard.id, "aaaaaaaa");
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Unknown);
 
         let shard_json_str = r#"
         {
-            "id": "aaaaaaaa",
+            "id": "shard_1",
+            "state": "unknown"
+        }
+        "#;
+        let shard_json_bytes = shard_json_str.as_bytes();
+        let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Unknown);
+
+        let shard_json_str = r#"
+        {
+            "id": "shard_1",
+            "state": "serving"
+        }
+        "#;
+        let shard_json_bytes = shard_json_str.as_bytes();
+        let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Serving);
+
+        let shard_json_str = r#"
+        {
+            "id": "shard_1",
+            "state": "draining"
+        }
+        "#;
+        let shard_json_bytes = shard_json_str.as_bytes();
+        let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Draining);
+
+        let shard_json_str = r#"
+        {
+            "id": "shard_1",
+            "state": "drained"
+        }
+        "#;
+        let shard_json_bytes = shard_json_str.as_bytes();
+        let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Drained);
+
+        let shard_json_str = r#"
+        {
+            "id": "shard_1",
             "version": 1
         }
         "#;
         let shard_json_bytes = shard_json_str.as_bytes();
-
         let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
-        assert_eq!(shard.id, "aaaaaaaa");
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Unknown);
+        assert_eq!(shard.version, 1);
+
+        let shard_json_str = r#"
+        {
+            "id": "shard_1",
+            "state": "serving",
+            "version": 1
+        }
+        "#;
+        let shard_json_bytes = shard_json_str.as_bytes();
+        let shard = serde_json::from_slice::<Shard>(shard_json_bytes).unwrap();
+        assert_eq!(shard.id, "shard_1");
+        assert_eq!(shard.state, State::Serving);
         assert_eq!(shard.version, 1);
     }
 }
